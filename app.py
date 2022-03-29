f10420c979203c62905cc73fce6c85bd7c21f84b
Katharina Adam <katharina.adam@bulme.at>
2022-03-29 10:28:06 +0200

    app

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask
-from flask.templating import render_template
+from flask import Flask, redirect
+from models import db 
 from flask_wtf.csrf import CSRFProtect
 
-import sqlalchemy
+
 app = Flask(__name__)
 app.secret_key = "VerySecretSecretKey"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/classicmodels"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/AbendVeranstaltungApp"
+db.init_app(app)
 
-csrf = CSRFProtect(app)
-
+#csrf = CSRFProtect(app)
 
 
 #hier blueprint registrieren
 
 
 app.run(debug=True)
+
