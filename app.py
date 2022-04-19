c36d3af6a54b84bd67dc7fd7154ad5e119de6a13
Katharina Adam <katharina.adam@bulme.at>
2022-04-19 09:56:00 +0200

    addes gast.py

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from Model.models import db
 from flask_wtf.csrf import CSRFProtect
-import sqlalchemy
 from controllers.index import index_blueprint
+from controllers.gast import gast_blueprint
 
+from flask_wtf.csrf import CSRFProtect
 
+import sqlalchemy
 app = Flask(__name__)
 app.secret_key = "VerySecretSecretKey"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/Abendveranstaltung"
+
+
+csrf = CSRFProtect(app)
+
 db.init_app(app)
 
 app.register_blueprint(index_blueprint)
+app.register_blueprint(gast_blueprint)
 
 app.run(debug=True)
